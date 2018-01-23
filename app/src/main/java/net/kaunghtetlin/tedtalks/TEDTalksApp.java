4736dd6d408c3a5a75f8a97da6cdc7f85e49f788
KaungHtetLin <kaunghtetlin74.khl@gmail.com>
2018-01-23 23:39:19 +0630

    project skeleton generation

---
+++ app/src/main/java/net/kaunghtetlin/tedtalks/TEDTalksApp.java
@@ -0,0 +1,13 @@
+package net.kaunghtetlin.tedtalks;
+
+import android.app.Application;
+
+/**
+ * Created by Kaung Htet Lin on 1/23/2018.
+ */
+
+public class TEDTalksApp extends Application {
+
+    public static final String LOG_TAG="TED-TALKs";
+
+}
